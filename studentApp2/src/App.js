c1bcce740e707672cbe3310cbd0abc112f7b9f97
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2024-04-02 13:13:50 -0400

    added microfrontend 2

---
+++ studentApp2/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/bootstrap/css/bootstrap.min.css";
+
+
+const App = () => (
+<section class="position-relative py-4 py-xl-5">
+        <div class="container position-relative">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+                    <div class="card mb-5">
+                        <div class="card-body p-sm-5">
+                            <h2 class="text-center mb-4">Student Info.</h2>
+                            <h6 class="text-center mb-4">(StudentApp1)</h6>
+                            <form method="post">
+                                <div class="mb-3"><input class="form-control" type="text" id="name-2" name="name" placeholder="Name"/></div>
+                                <div class="mb-3"><input class="form-control" type="email" id="email-2" name="email" placeholder="Email"/></div>
+                                <input class="form-control" type="text" name="phone" placeholder="phone number" required="" minlength="10" maxlength="10"/>
+                                <div class="mb-3"></div>
+                                <div><button class="btn btn-primary d-block w-100" type="submit">Send </button></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
